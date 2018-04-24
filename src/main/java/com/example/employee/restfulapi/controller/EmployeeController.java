0a2ee46107f4dd165ddb1827c1fef11b0dedc3b8
htao <htao@thoughtworks.com>
2018-04-24 16:31:30 +0800

    [taohui] get all employees page by page

--- src/main/java/com/example/employee/restfulapi/controller/EmployeeController.java
+++ src/main/java/com/example/employee/restfulapi/controller/EmployeeController.java
@@ -3,7 +3,11 @@ package com.example.employee.restfulapi.controller;
 import com.example.employee.restfulapi.entity.Employee;
 import com.example.employee.restfulapi.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -22,5 +26,10 @@ public class EmployeeController {
     @GetMapping("/employees/{id}")
     public Employee GetEmployeeById(@PathVariable long id){
         return employeeRepository.findOne(id);
+    }
+
+    @GetMapping("/employees/page/{page}/pageSize/{pageSize}")
+    public Page<Employee> GetEmployeesByPage(@PathVariable int page, @PathVariable int pageSize){
+        return employeeRepository.findAll(new PageRequest(page, pageSize));
     }
 }
